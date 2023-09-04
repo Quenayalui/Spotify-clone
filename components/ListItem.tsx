79a3935c4df2a454f7ddd681f5c9d54aef3f4b12
lui <quenaya@web.de>
2023-09-04 14:49:29 +0200

    fished general layout started authentication

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { FaPlay } from "react-icons/fa";
 
@@ -16,7 +16,10 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
     router.push(href);
   };
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+      onClick={onClick}
+    >
       <div className="relative min-h-[64px] min-w-[64] ">
         <Image className="object-cover" fill src={image} alt="Image" />
       </div>
